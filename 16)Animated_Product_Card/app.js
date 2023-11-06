02407dc33e753dc8ff088319329e9127c990eb1d
777beybey <beyzaselvi777@gmail.com>
2023-11-06 22:33:12 +0300

    animation
    cards animation

---
+++ 16)Animated_Product_Card/app.js
@@ -0,0 +1,9 @@
+// BU PROJE REACT İLE YAPILDI O YÜZDEN REACT ÖĞRENİNCE BU PROJEYE GERİ DÖN JS
+// PART INI ÖYLE ANLAMAYA ÇALIŞ.
+// Animated Product Card using HTML CSS and JavaScript
+// CodingLab
+// 15.20
+
+
+
+
